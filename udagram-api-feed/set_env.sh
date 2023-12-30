8ecb629a24650f5e9a2c4dc4bb4b4c095e1f4cf7
nguyenhuudamm <nguyenhuudam98@gmail.com>
2023-12-31 00:28:15 +0700

    check

---
+++ udagram-api-feed/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cwdcgwczxyes.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::clouddev-2565-9000-0941-microvervices
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+
